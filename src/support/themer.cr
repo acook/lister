ed677d3fc290003bea494d5966c41eb254f22169
Anthony Cook <github@anthonymcook.com>
2016-12-25 23:13:09 -0500

    Persist themes, dedicated Theme object.

--- src/support/themer.cr
+++ src/support/themer.cr
@@ -1,3 +1,5 @@
+require "yaml"
+
 module Themer
   def self.create
     td = ThemeDefiner.new
@@ -9,14 +11,13 @@ module Themer
   alias STYLE = Symbol | Nil
 
   class Colors
-    property codes : String
+    property codes : String = ""
 
-    def initialize(bg : COLOR = nil, fg : COLOR = nil, style : STYLE = nil)
+    def set(bg : COLOR = nil, fg : COLOR = nil, style : STYLE = nil)
       @codes = "\033["
 
       if style
-        @codes += s style
-        @codes +=  ";"
+        @codes += "#{s style};"
       end
 
       case bg
@@ -38,6 +39,8 @@ module Themer
       end
 
       @codes += "m"
+
+      self # return self for chaining
     end
 
     def to_s(io)
@@ -60,24 +63,72 @@ module Themer
       "#{r};#{g};#{b}"
     end
 
+    def to_yaml(*args)
+      codes.to_yaml *args
+    end
+
     COLORS = %i[black red green yellow blue magenta cyan white extended default]
     STYLES = %i[normal bold faint italic underlined blink blink_fast inverse conceal crossed_out]
   end
 
   class ThemeDefiner
-    property theme = Hash(Symbol, Colors).new
+    property theme = Theme.new
 
-    def for(id : Symbol, bg : COLOR = nil, fg : COLOR = nil, style : STYLE = nil)
-      colors = Colors.new bg: bg, fg: fg, style: style
+    def for(id : String, bg : COLOR = nil, fg : COLOR = nil, style : STYLE = nil)
+      colors = Colors.new.set bg: bg, fg: fg, style: style
       @theme[id] = colors
       @theme
     end
   end
+
+  class Theme
+    property store = Hash(String, Colors).new
+    property reset : Colors = Colors.new.set style: :normal
+
+    def self.load(filename)
+      yaml = File.open filename do |file|
+        YAML.parse(file)
+      end
+
+      data = Hash(String, Colors).new
+      yaml.as_h.each do |k,v|
+        data[k.to_s] = Colors.new.tap{|c| c.codes = v.to_s }
+      end
+
+      self.new.tap{|t| t.store = data }
+    end
+
+    def save(filename)
+      File.open filename, mode: "w" do |file|
+        file.puts YAML.dump(store)
+      end
+    end
+
+    def [](id : String) : Colors | NoReturn
+      store[id] || raise ArgumentError.new("id not found #{id.inspect}")
+    end
+
+    def []=(id : String, seq : Colors)
+      store[id] = seq || raise ArgumentError.new("key cannot be nil")
+    end
+  end
 end
 
-theme = Themer.create do |t|
-  t.for :foo, bg: :red
-  t.for :bar, fg: "#de1e7e"
+theme = Themer.create do
+  for "reset", style: :normal
+  for "err", bg: :red, style: :bold, fg: :white
+  for "lookatme", fg: "#de1e7e", style: :bold
+  for "foo", fg: "#BADA55"
+  for "bar", fg: "#e1e100"
+  for "baz", fg: "#c0ffee"
+  for "qux", fg: "#1CE1CE"
 end
 
-print theme[:bar], "foooooo", "\033[0m\n"
+theme.save "mytheme.theme"
+theme = Themer::Theme.load "mytheme.theme"
+
+print theme["err"],      "DANGER WILL ROBINSON DANGER DANGER", theme.reset, '\n'
+print theme["lookatme"], "EXTERMINATE ANNIHILATE DESTROY",     theme.reset, '\n'
+print theme["foo"], "foo ", theme["bar"], "bar ", theme["baz"], "baz ", theme["qux"], "qux "
+print theme.reset, '\n'
+
