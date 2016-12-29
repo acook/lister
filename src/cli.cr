414aabf73d55596c070000a46dbd1b9164faa5b8
Anthony Cook <github@anthonymcook.com>
2016-12-29 00:31:32 -0500

    Commenting and reorganizing CLI a bit.

--- src/cli.cr
+++ src/cli.cr
@@ -10,6 +10,25 @@ module Lister
     property options = Options.new
     property paths = Array(String).new
 
+    # this is the method which starts the ball rolling
+    # but if #parse_args hasn't been run it will implode
+    def run
+      longest = paths.map{|path| path.size }.max
+
+      paths.each do |path|
+        Formatter.new(wrap(path), options).render longest
+      end
+
+    ensure
+      # make sure we don't make a mess of the terminal colors
+      print Themer.reset
+      # HACK: The following is a result of a major bug in Crystal
+      # https://github.com/crystal-lang/crystal/issues/2065
+      STDOUT.blocking = true
+      STDERR.blocking = true
+    end
+
+    # this does a hack job of parsing commandline arguments
     def parse_args(args)
       skip = nil
       args.each.with_index do |arg, i|
@@ -25,25 +44,12 @@ module Lister
         end
       end
 
+      # search the current directory if nothing else
       @paths << "." if @paths.empty?
 
       self # make chainable since there is no meaningful return value
     end
 
-    def run
-      longest = paths.map{|path| path.size }.max
-
-      paths.each do |path|
-        Formatter.new(wrap(path), options).render longest
-      end
-
-    ensure
-      print Themer.reset
-      # HACK: The following is a result of a major bug in Crystal
-      # https://github.com/crystal-lang/crystal/issues/2065
-      STDOUT.blocking = true
-      STDERR.blocking = true
-    end
 
     def wrap(path)
       if File.directory? path
@@ -68,22 +74,22 @@ module Lister
     end
   end
 
+  # the Options object is essentially an environment which gets passed around
+  #   in lieu of globals or tons of extraneous arguments
+  # this object stores the commandline options and the interface to libmagic
   class Options
     property recurse : Int8 = 1_i8
     property magic : Magic::Magic
     property terminal = Terminal.new
 
     def initialize
-      # the default flag sets it to onl return MIME types,
-      # which is an extremely limited subset of possible types
+      # the default flags set it to only return MIME types,
+      #   which is an extremely limited subset of possible types
+      #   the below line could be used instead, but
+      #   the current flags also search compressed files
       #flags = Magic::LibMagic::Flags::NONE.to_i
-      # this version also search inside compressed files
       flags = Magic::LibMagic::Flags::COMPRESS.to_i
       @magic = Magic::Magic.new flags: flags
-    end
-
-    def recurse=(depth)
-      @recurse = depth
     end
 
     def recurse?
