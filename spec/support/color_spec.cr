ceb9caf86dc486632a3206fda9a9ae56f2292824
Anthony Cook <github@anthonymcook.com>
2021-02-12 15:14:30 -0600

    Use should_not eq instead of should be_false.
    Also adds a little more to the test for color depth fallback.

--- spec/support/color_spec.cr
+++ spec/support/color_spec.cr
@@ -14,7 +14,7 @@ describe Themer::Color do
     color1 = klass.new.set style16: "bold", fg16: "white", bg16: "red"
     color2 = klass.new.set style16: "none", fg16: "white", bg16: "red"
 
-    (color1 == color2).should be_false
+    color1.should_not eq color2
   end
 
   it "falls back to lower color depth colors if higher ones aren't set" do
@@ -23,8 +23,9 @@ describe Themer::Color do
   end
 
   it "does not fall back to lower depth colors if set to 'none'" do
-    color = klass.new.set fg: "none", fg16: "red"
-    color.codes_for(16_000_000).should eq("")
+    color = klass.new.set fg: "none", bg: "#BADA55", fg16: "red", bg16: "green"
+    color.codes_for(16_000_000).should_not eq colors.codes_for(16)
+    color.codes_for(16_000_000).should eq("\e[48;2;186;218;85m")
   end
 
   describe "color codes" do
