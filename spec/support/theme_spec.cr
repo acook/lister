b1e2d7732eff2da4704bd1ccc374f7a4844384b8
Anthony Cook <github@anthonymcook.com>
2021-02-12 14:45:14 -0600

    Group Theme#default tests.

--- spec/support/theme_spec.cr
+++ spec/support/theme_spec.cr
@@ -3,32 +3,34 @@ require "./spec_helper"
 describe Themer::Theme do
   klass = Themer::Theme
 
-  it "allows default to be nil" do
-    theme = klass.new
-
-    theme.default.should be_nil
-  end
-
-  it "falls back to default if ID not found" do
-    theme = klass.new
-    theme.default = Themer::Color.new.set style: "italic"
-    (default = theme.default).nil? && fail "default should never be nil"
-
-    theme.for("unknown").codes.should eq(default.codes)
-  end
-
-  it "has codes for the default entry" do
-    theme = klass.new
-    theme.default = Themer::Color.new.set style: "italic"
-    (default = theme.default).nil? && fail "default should never be nil"
-
-    default.codes.should eq "\e[3m"
-  end
-
   it "provides handy access to reset codes" do
     theme = klass.new
 
     theme.reset.codes.should eq "\e[0m"
+  end
+
+  describe "#default" do
+    it "allows default to be nil" do
+      theme = klass.new
+
+      theme.default.should be_nil
+    end
+
+    it "falls back to default if ID not found" do
+      theme = klass.new
+      theme.default = Themer::Color.new.set style: "italic"
+      (default = theme.default).nil? && fail "default should never be nil"
+
+      theme.for("unknown").codes.should eq(default.codes)
+    end
+
+    it "has codes for the default entry" do
+      theme = klass.new
+      theme.default = Themer::Color.new.set style: "italic"
+      (default = theme.default).nil? && fail "default should never be nil"
+
+      default.codes.should eq "\e[3m"
+    end
   end
 
   describe "#for with arrays" do
