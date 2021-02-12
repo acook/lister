6720704516e4e259697daf1955e85a2a7acd61d2
Anthony Cook <github@anthonymcook.com>
2021-02-12 15:13:35 -0600

    Test each color depth produces correct codes.

--- spec/support/color_spec.cr
+++ spec/support/color_spec.cr
@@ -26,4 +26,26 @@ describe Themer::Color do
     color = klass.new.set fg: "none", fg16: "red"
     color.codes_for(16_000_000).should eq("")
   end
+
+  describe "color codes" do
+    it "produces correct normal/reset codes" do
+      colorreset = klass.new.set style16: "normal"
+      colorreset.codes.should eq "\e[0m"
+    end
+
+    it "produces correct 16-color codes" do
+      color16 = klass.new.set bg16: "red", style16: "bold", fg16: "white"
+      color16.codes.should eq "\e[1;37;41m"
+    end
+
+    it "produces correct 256-color codes" do
+      color256 = klass.new.set bg256: "33"
+      color256.codes.should eq "\e[48;5;33m"
+    end
+
+    it "produces correct true-color codes" do
+      colortrue = klass.new.set fg: "#1CE1CE", bg: "#5E1F1E"
+      colortrue.codes.should eq "\e[38;2;28;225;206m\e[48;2;94;31;30m"
+    end
+  end
 end
--- spec/support/themer_spec.cr
+++ spec/support/themer_spec.cr
@@ -1,29 +1,4 @@
 require "./spec_helper"
 
 describe Themer do
-  test_theme = Themer.build do
-    default style: "italic"
-    # style only
-    for "reset", style: "normal"
-    # 16 colors
-    for "err", bg16: "red", style16: "bold", fg16: "white"
-    # 256 color
-    for "thehellofit", bg256: "33"
-    # true color
-    for "lookatme", fg: "#de1e7e", style: "bold"
-    for "foo", fg: "#BADA55"
-    for "bar", fg: "#e1e100"
-    for "baz", fg: "#c0ffee"
-    for "qux", fg: "#1CE1CE"
-
-    for "hasnoneset", fg: "none", fg16: "red"
-  end
-
-  it "does all the things" do
-    theme = test_theme
-
-    theme.for("err").codes.should eq "\e[1;37;41m"
-    theme["thehellofit"].codes.should eq "\e[48;5;33m"
-    theme["lookatme"].codes.should eq "\e[1;38;2;222;30;126m"
-  end
 end
