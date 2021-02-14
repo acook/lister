fa06da9bc96c8df5c0055ce4365126a11931ea08
Anthony Cook <github@anthonymcook.com>
2021-02-14 00:19:38 -0600

    Remove workarounds for old Crystal bug.
    Fixed back in 2018 by this PR: https://github.com/crystal-lang/crystal/pull/6518

--- src/lister.cr
+++ src/lister.cr
@@ -6,8 +6,4 @@ begin
 ensure
   # make sure we don't make a mess of the terminal colors
   print "\e[0m"
-  # HACK: The following is a result of a major bug in Crystal
-  # https://github.com/crystal-lang/crystal/issues/2065
-  STDOUT.blocking = true
-  STDERR.blocking = true
 end
